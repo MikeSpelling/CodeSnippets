e0086dac5f01292f9a8651292b25b5542e0dac93
Michael Atkins-Spelling <michael.atkins-spelling@footasylum.com>
2023-04-08 06:47:03 +0100

    Update playground so its just empty for testing if wanted - doesn't actually use the code snippets

--- CodeSnippets.playground/Contents.swift
+++ CodeSnippets.playground/Contents.swift
@@ -1,18 +1,7 @@
 import UIKit
 
-let data = try! JSONSerialization.data(withJSONObject: ["key": "value"])
-if let json = (try? JSONSerialization.jsonObject(with: data)) as? [String: Any] {
-    print(json)
-}
+print("Go nuts...")
 
-private enum CodingKeys: String, CodingKey {
-    case name = "name"
-}
-public init(from decoder: Decoder) throws {
-    let container = try decoder.container(keyedBy: CodingKeys.self)
-    self.name = try container.decode(String.self, forKey: .name)
-}
-public func encode(to encoder: Encoder) throws {
-    var container = encoder.container(keyedBy: CodingKeys.self)
-    try container.encode(name, forKey: .name)
-}
+let goNuts = "Go Nuts"
+
+print(goNuts)
